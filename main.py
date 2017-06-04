38666d0aedf96aaaeb0c36951c5caacbaf7c91d5
Ryan Harrison <mail@ryanharrison.co.uk>
2017-06-04 20:38:00 +0100

    Add blog base dir

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         base_dir = str(sys.argv[1])
     else:
-        base_dir = expanduser("~")
+        base_dir = expanduser("~") + "/blog/_site"
 
     print("Finding posts in %s" % base_dir)
 
