9669145798f636fba1a0c00801902c2c8a0b61a3
Ryan Harrison <mail@ryanharrison.co.uk>
2017-06-04 20:37:04 +0100

    Properly expand home directory

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
+from os.path import expanduser
 from find_posts import create_posts
 import indexer
-
-base_dir = "~/blog/_site"
 
 if __name__ == "__main__":
     # provide blog base directory as arg
     if len(sys.argv) > 1:
         base_dir = str(sys.argv[1])
+    else:
+        base_dir = expanduser("~")
 
     print("Finding posts in %s" % base_dir)
 
